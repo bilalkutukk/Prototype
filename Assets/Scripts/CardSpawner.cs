087e39efbeff4062e1beb3fa905e2392017f0e48
Bilal Kutuk <bilalkutuk@outlook.com>
2025-04-19 15:23:23 +0300

    stuff

--- Assets/Scripts/CardSpawner.cs
+++ Assets/Scripts/CardSpawner.cs
@@ -1,18 +1,41 @@
 // Scripts/CardSpawner.cs
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CardSpawner : MonoBehaviour
 {
-    public GameObject cardPrefab;
-    public Transform spawnPoint;
-    public CardData testCard;
+public GameObject cardPrefab;
+    public Transform marketArea;
+    public Transform playerHand;
+    public List<CardData> marketCards;
+    public List<CardData> playerCards;
+    public float spacing = 200f;
 
     void Start()
     {
-        GameObject card = Instantiate(cardPrefab, spawnPoint.localPosition, Quaternion.identity);
-        card.transform.SetParent(spawnPoint, false);
-        card.GetComponent<CardDisplay>().LoadCard(testCard);
+        CardData aCard = Resources.Load<CardData>("Cards/PotionCard");
+        CardData bCard = Resources.Load<CardData>("Cards/PotionCard");
 
-        Debug.Log("Card spawn point x and y: " + spawnPoint.position.x + ", " + spawnPoint.position.y);
+        marketCards.Add(aCard);
+        marketCards.Add(bCard);
+
+        playerCards.Add(aCard);
+
+        SpawnCards(marketCards, marketArea);
+        SpawnCards(playerCards, playerHand);
+    }
+
+    void SpawnCards(List<CardData> cards, Transform parent)
+    {
+        for (int i = 0; i < cards.Count; i++)
+        {
+            GameObject card = Instantiate(cardPrefab);
+            card.transform.SetParent(parent, false);
+
+            RectTransform rt = card.GetComponent<RectTransform>();
+            rt.anchoredPosition = new Vector2(i * spacing, 0);
+
+            card.GetComponent<CardDisplay>().LoadCard(cards[i]);
+        }
     }
 }
